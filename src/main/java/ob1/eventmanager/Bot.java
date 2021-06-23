67be9dcc5a37a3262f0d022b83447c463ee106d0
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-06-23 19:52:50 +0300

    changed buttons (not working)

--- src/main/java/ob1/eventmanager/Bot.java
+++ src/main/java/ob1/eventmanager/Bot.java
@@ -1,17 +1,28 @@
 package ob1.eventmanager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 @Component
 public class Bot extends TelegramLongPollingBot {
 
-    @Value("${telegram.bot.username}")
+    @Value("Event Confirmation")
     private String botUsername;
 
     @Value("${telegram.bot.token}")
@@ -35,6 +46,40 @@ public class Bot extends TelegramLongPollingBot {
         final String text = message.getText();
         sendMessage.setText(text);
         sendMessage.setChatId(String.valueOf(message.getChatId()));
+
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Тык");
+        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
+        inlineKeyboardButton2.setText("Тык2");
+        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+
+
+//
+//        final EventConfirmation eventConfirmation = new EventConfirmation();
+//
+//        final KeyboardRow keyboardButtons = eventConfirmation.showButtons();
+//        final KeyboardRow keyboardButtons = new KeyboardRow();
+//        keyboardButtons.add("всё верно ✔️");
+//        keyboardButtons.add("отменить");
+
+
+//        sendMessage.setReplyMarkup(new ReplyKeyboardMarkup(
+//                Arrays.asList(
+//                        keyboardButtons
+//                )
+//        ));
 
         try {
             execute(sendMessage);
