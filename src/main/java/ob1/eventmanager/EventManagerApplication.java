dcfe108f166f4c0824f3c7b10d11ddcbac540823
ya-evgeniy <jekarus47@gmail.com>
2021-06-23 08:05:11 +0300

    Initial commit

---
+++ src/main/java/ob1/eventmanager/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package ob1.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+}
