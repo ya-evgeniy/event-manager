18b7752d7f8ac1978348791f81fb9a399dc96522
ya-evgeniy <jekarus47@gmail.com>
2021-06-23 17:10:25 +0300

    Usage telegram bot api example

---
+++ src/main/java/ob1/eventmanager/Bot.java
@@ -0,0 +1,46 @@
+package ob1.eventmanager;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        final SendMessage sendMessage = new SendMessage();
+
+        final Message message = update.getMessage();
+        final String text = message.getText();
+        sendMessage.setText(text);
+        sendMessage.setChatId(String.valueOf(message.getChatId()));
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/ob1/eventmanager/EventManagerApplication.java
+++ src/main/java/ob1/eventmanager/EventManagerApplication.java
@@ -2,8 +2,14 @@ package ob1.eventmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class},
+        scanBasePackages = {"ob1.eventmanager"}
+)
 public class EventManagerApplication {
 
     public static void main(String[] args) {
