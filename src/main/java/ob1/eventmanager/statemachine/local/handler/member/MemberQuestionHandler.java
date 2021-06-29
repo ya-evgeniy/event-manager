ef15c24894515dedca3cf79dc71fc560eba6382a
L-san <lsarelli9402@gmail.com>
2021-06-29 17:50:25 +0400

    MemberQuestionHandler

--- src/main/java/ob1/eventmanager/statemachine/local/handler/member/MemberQuestionHandler.java
+++ src/main/java/ob1/eventmanager/statemachine/local/handler/member/MemberQuestionHandler.java
@@ -3,16 +3,16 @@ package ob1.eventmanager.statemachine.local.handler.member;
 import ob1.eventmanager.bot.TelegramBot;
 import ob1.eventmanager.entity.EventQuestionEntity;
 import ob1.eventmanager.entity.MemberEntity;
-import ob1.eventmanager.service.EventService;
+import ob1.eventmanager.service.EventQuestionService;
 import ob1.eventmanager.service.MemberAnswerService;
+import ob1.eventmanager.service.MemberService;
 import ob1.eventmanager.statemachine.MessageStateMachineContext;
 import ob1.eventmanager.statemachine.MessageStateMachineHandler;
 import ob1.eventmanager.statemachine.local.LocalChatStates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @Component("localMemberQuestionHandler")
 public class MemberQuestionHandler implements MessageStateMachineHandler<LocalChatStates> {
@@ -24,33 +24,32 @@ public class MemberQuestionHandler implements MessageStateMachineHandler<LocalCh
     private MemberAnswerService memberAnswerService;
 
     @Autowired
-    private EventService eventService;
+    private EventQuestionService eventQuestionService;
 
-    private int prevQuestionIndex = 0;
+    @Autowired
+    private MemberService memberService;
 
     @Override
     public void handle(MessageStateMachineContext<LocalChatStates> context) {
         final String text = context.get("text");
         final String chatId = context.get("chatId");
-        final String callbackQuery = context.get("callbackData");
-        final int messageId = context.get("messageId");
-        final MemberEntity member = new MemberEntity();//fixme добавить getMemberEntity()
-//        List<EventQuestionEntity> questions = eventService.getGroupEvent(Long.parseLong(chatId)).getQuestions(); // fixme
-        List<EventQuestionEntity> questions = new ArrayList<>();
-        final String currentQuestion = questions.get(prevQuestionIndex).getQuestion();
+        final MemberEntity member = new MemberEntity();//fixme from context
 
         final LocalChatStates previousState = context.getPreviousState();
 
-        if (previousState == LocalChatStates.MEMBER_DATE) {
-            bot.send(currentQuestion,chatId);
-            prevQuestionIndex+=1;
-            //todo добавить вывод вопросов
-
-        } else if (previousState == LocalChatStates.MEMBER_QUESTION) {
-
-                context.setNextState(LocalChatStates.MEMBER_CONFIRM);
-        } else {
+        if (previousState != LocalChatStates.MEMBER_DATE && previousState != LocalChatStates.MEMBER_QUESTION) {
             throw new UnsupportedOperationException(previousState.name() + " -> " + context.getCurrentState());
         }
+        if(member.getCurrentQuestion()!=null){
+            memberAnswerService.setAnswer(member,text);
+        }
+        Optional<EventQuestionEntity> currentQuestion = eventQuestionService.getUnansweredQuestion(member);
+        if (currentQuestion != null) {
+            bot.send(currentQuestion.get().getQuestion(), chatId);
+        } else {
+            bot.send("Ура, мой список вопросов закончился!", chatId);
+            context.setNextState(LocalChatStates.MEMBER_CONFIRM);
+        }
     }
+
 }
