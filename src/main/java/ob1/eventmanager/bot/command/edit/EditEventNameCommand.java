9728568192ec3ea22aea923f9179894b5d4a5cb5
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-06-30 21:53:36 +0300

    added edit name & edit place commands

--- src/main/java/ob1/eventmanager/bot/command/edit/EditEventNameCommand.java
+++ src/main/java/ob1/eventmanager/bot/command/edit/EditEventNameCommand.java
@@ -3,10 +3,15 @@ package ob1.eventmanager.bot.command.edit;
 import ob1.eventmanager.bot.TelegramBot;
 import ob1.eventmanager.bot.ann.LocalCommand;
 import ob1.eventmanager.bot.command.LocalCommandHandler;
+import ob1.eventmanager.entity.EventEntity;
 import ob1.eventmanager.entity.UserEntity;
+import ob1.eventmanager.exception.EventNotFoundException;
+import ob1.eventmanager.service.EventService;
+import ob1.eventmanager.service.UserService;
 import ob1.eventmanager.statemachine.MessageStateMachine;
 import ob1.eventmanager.statemachine.local.LocalChatStates;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 
 import java.util.Map;
 
@@ -15,6 +20,12 @@ public class EditEventNameCommand implements LocalCommandHandler {
 
     @Autowired
     private TelegramBot bot;
+
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public void handle(MessageStateMachine<LocalChatStates> stateMachine, Map<String, Object> headers) {
@@ -29,8 +40,32 @@ public class EditEventNameCommand implements LocalCommandHandler {
         }
 
         UserEntity user = (UserEntity) headers.get("user");
-        long chatIdLong = (long) headers.get("chatIdLong");
 
+        final String[] commandArgs = (String[]) headers.get("commandArgs");
+
+        if (commandArgs.length == 0) {
+            bot.send("Выбранное мероприятие не найдено, попробуй выбрать другое.", chatId);
+            return;
+        }
+
+        EventEntity event;
+        try {
+            long eventId = Long.parseLong(commandArgs[0]);
+            event = eventService.getEventById(eventId);
+        }
+        catch (NumberFormatException | EventNotFoundException e) {
+            bot.send("Выбранное мероприятие не найдено, попробуй выбрать другое.", chatId);
+            return;
+        }
+
+        if (event.getOwner().getId() != user.getId()) {
+            bot.send("Мероприятие не пренадлежит тебе, ты не можешь его изменять!", chatId);
+            return;
+        }
+
+        userService.setUserSelectedEvent(user, event);
+        stateMachine.setCurrentState(LocalChatStates.EDIT_EVENT_NAME);
+        stateMachine.handle(headers);
     }
 
 }
